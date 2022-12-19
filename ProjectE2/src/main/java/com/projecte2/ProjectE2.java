3bbf2337f743374d23207627be585cbbde30c008
= <quan.nd.1934@aptechlearning.edu.vn>
2022-12-19 17:37:40 +0700

    commit

---
+++ ProjectE2/src/main/java/com/projecte2/ProjectE2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.projecte2;
+
+/**
+ *
+ * @author Lenovvo
+ */
+public class ProjectE2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
